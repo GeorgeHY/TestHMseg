2773c4bd63b968d4ac23fc1fed06dcd51056e7be
GeorgeHY <hanyangkid@163.com>
2015-05-01 15:05:31 +0800

    Initial Commit

---
+++ TestHMseg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestHMseg
+//
+//  Created by 韩扬 on 15/5/1.
+//  Copyright (c) 2015年 iwind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestHMseg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestHMseg
+//
+//  Created by 韩扬 on 15/5/1.
+//  Copyright (c) 2015年 iwind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
